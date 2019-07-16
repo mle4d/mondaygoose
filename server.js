91503df6652f3411cb7503105382a388569b00ee
emily <mle4d@me.com>
2019-07-16 11:58:28 -0700

    connect server and middleware

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./lib/utils/connect')();
 const app = require('./lib/app');
 const PORT = process.env.PORT || 7890;
 
-app.lsten(PORT, () => {
+app.listen(PORT, () => {
   console.log(`started on ${PORT}`);
 });
 
